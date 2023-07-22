9f8eb4778cf30551e1e8484d68ea3c13da86dc38
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-07-22 15:02:14 +0200

    Aggiungere i file di progetto.

---
+++ SOLID Review the queue,  Open -Closed/IMyQueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Review_the_queue___Open__Closed
+{
+    public interface IMyQueue<T>
+    {
+        public void Queue(T value);
+        public T Dequeue();
+        public T Peek();
+    }
+}
---
+++ SOLID Review the queue,  Open -Closed/Program.cs
@@ -0,0 +1,31 @@
+﻿using SOLID_Review_the_queue___Open__Closed;
+
+var myQueue = new MyQueue<int>();
+
+myQueue.Queue(1);
+myQueue.Queue(2);
+myQueue.Queue(3);
+myQueue.Queue(4);
+myQueue.Queue(5);
+
+Console.WriteLine("sto stampando i dati dalla queue:");
+foreach (int item in myQueue)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("sto prendendo l'ultimo elemento dalla queue");
+var result1 = myQueue.Peek();
+Console.WriteLine(result1);
+
+
+Console.WriteLine("sto eleminando l'ultimo elemento dalla queue");
+var result2 = myQueue.Dequeue();
+Console.WriteLine(result2);
+
+Console.WriteLine("sto stampando i restanti dati");
+
+foreach (int item in myQueue)
+{
+    Console.WriteLine(item);
+}
